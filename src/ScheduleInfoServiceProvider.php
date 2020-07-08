ed339de1b478e00d6fc06151228e6fc397437f21
Tobias Lang <tlang@fintecsystems.com>
2020-07-08 16:31:31 +0200

    Fix command register.

--- src/ScheduleInfoServiceProvider.php
+++ src/ScheduleInfoServiceProvider.php
@@ -7,45 +7,16 @@ use Illuminate\Support\ServiceProvider;
 class ScheduleInfoServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Bootstrap the application events.
      *
      * @return void
      */
     public function boot()
     {
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->instance(
-            'command.schedule.info',
-            ScheduleInfoCommand::class
-        );
-
-        $this->commands(
-            'command.schedule.info'
-        );
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['command.schedule.info'];
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ScheduleInfoCommand::class,
+            ]);
+        }
     }
 }
